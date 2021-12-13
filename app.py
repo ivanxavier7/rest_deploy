407bad790da694b052c5122c4239ff37b0b6460e
Ivan Xavier <ivanxavier123@gmail.com>
2021-12-13 14:15:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -47,13 +47,25 @@ def invalid_token_callbak(error):
                     'error': 'invalid_token'}),401
 
 @jwt.unauthorized_loader
-
+def missing_token_callback(error):
+    return jsonify({
+        "description": "Request does not contain an access token.",
+        'error': 'authorization_required'
+    }), 401
 
 @jwt.needs_fresh_token_loader
-
+def token_not_fresh_callback(jwt_header, jwt_payload):
+    return jsonify({
+        "description": "The token is not fresh.",
+        'error': 'fresh_token_required'
+    }), 401
 
 @jwt.revoked_token_loader
-
+def revoked_token_callback(jwt_header, jwt_payload):
+    return jsonify({
+        "description": "The token has been revoked.",
+        'error': 'token_revoked'
+    }), 401
 
 
 api.add_resource(Store, '/store/<string:name>')
